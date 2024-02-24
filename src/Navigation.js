26d3a6c278fa5725663067cc7b12ceaab9097590
Gevin Sarmiento <114702605+n1cerium@users.noreply.github.com>
2024-02-23 23:18:44 -0800

    nav changes

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,3 +1,9 @@
 export default function Navigation() {
-  return <nav id="navigation">this is nav</nav>;
+  return (
+    <nav id="navigation">
+      <h3> Plask </h3>
+      <h3> Time </h3>
+      <h3> Weather</h3>
+    </nav>
+  );
 }
