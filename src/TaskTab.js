58cf12d4dd093bbb8619c99b40e74afb4805849b
Gevin Sarmiento <114702605+n1cerium@users.noreply.github.com>
2024-02-24 21:12:35 -0800

    task tab animation and adding list

--- src/TaskTab.js
+++ src/TaskTab.js
@@ -1,3 +1,51 @@
+import { useEffect, useState } from "react";
+
 export default function TaskTab() {
-  return <ul className="task-tab"></ul>;
+  return (
+    <ul id="task-tab">
+      {Array.from({ length: 15 }, (_, i) => i + 1).map((num) => (
+        <TaskDatesList key={num} />
+      ))}
+    </ul>
+  );
+}
+function TaskDatesList() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isRendered, setIsRendered] = useState(false);
+
+  function handleOpenMain() {
+    setIsOpen((o) => !o);
+  }
+  // delaying the unmount of TaskList component so it performs the closing animation
+  useEffect(() => {
+    let timerID;
+    //if the TaskList component is closing and is currently rendered
+    //set isRendered to false after 0.5 seconds
+    if (!isOpen && isRendered) {
+      timerID = setTimeout(() => setIsRendered(false), 200);
+    } else if (isOpen && !isRendered) {
+      //if TaskList is opening and not currently rendered on the screen
+      // set isRendered to true so the TaskList component will be rendered on the screen
+      setIsRendered(true);
+    }
+
+    return () => clearTimeout(timerID);
+  }, [isOpen, isRendered]);
+  return (
+    <li>
+      <header className="task-header">
+        <p>Header</p>
+        <span onClick={handleOpenMain}>{isOpen ? "-" : "+"}</span>
+      </header>
+      {isRendered && (
+        <TaskList
+          className={isOpen ? "task-list-open" : "task-list-close"}
+          isOpen={isOpen}
+        />
+      )}
+    </li>
+  );
+}
+function TaskList({ className, isOpen }) {
+  return <main className={className}>{isOpen ? "Main Content" : ""}</main>;
 }
